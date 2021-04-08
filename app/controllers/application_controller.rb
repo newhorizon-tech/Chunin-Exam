f762f7a266ae22e1c1d146d6c03c34a4f3b94a29
newhorizon-tech <alaukikinbox@gmail.com>
2021-04-08 20:50:39 +0530

    Fix URL redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   def generate_url
     SecureRandom.urlsafe_base64(10).gsub(/-|_/,('a'..'z').to_a[rand(26)])
   end
+
+  def clean_url(url)
+    u = URI.parse(url)
+    return url if(%w{http https}.include?(u.scheme))
+
+    return 'http://' + u.to_s
+  end
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,18 +1,18 @@
 class LinksController < ApplicationController
   def index
-    @links = Link.all
   end
 
   def create
     @link = Link.new(link_params)
+    @link.full_url = clean_url(@link.full_url.strip)
     @link.short_url = generate_url()
     @link.save!
     render @link
   end
 
   def short_redirect
-   @link = Link.find_by(short_url: params[:short_url])
-   redirect_to @link.full_url
+   @redirect_link = Link.find_by(short_url: params[:short_url])
+   redirect_to @redirect_link.full_url
   end
 
   private
